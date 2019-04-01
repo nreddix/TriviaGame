feebe9508b9cc3548a0fb77fefd3bc9cfcd6ecd3
Nia Reddix <niareddix@Laceys-MacBook-Pro.local>
2019-04-01 18:52:15 -0400

    updated JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,3 @@
-var delayButtonAlert;
 var questions = [
 "The color orange is named after the fruit" ,
 "You can legally drink alcohol while driving in Mississippi" ,
@@ -6,18 +5,12 @@ var questions = [
 "Google was originally called Backrub"
 ]
 
+var windowAlert = setTimeout (function() {
+alert("Let the Games Begin!");
+}, 1000);
 
+for (var i = 0; i < questions.length; i++) {
 $("#Play").on("click", function() {
-    delayButtonAlert = setTimeout(function() {
-    }, 2000);
-alert("The color orange is named after the fruit.");
-console.log(delayButtonAlert)
-
- function play() {
-  for (var i = 0; i < questions.length; i++) {
-     $("#question").text(questions[i]);
-   }}
-
-
-
-})
+  $("#questions").append("<h2>The color orange is named after the fruit</h2>");
+}
+)}
