cf49fe91a52212f75f29786a5fa542bee4e69f69
Nia Reddix <niareddix@Laceys-MacBook-Pro.local>
2019-04-04 18:36:18 -0400

    updated JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,10 +3,13 @@ var windowAlert = setTimeout (function() {
 alert("Let the Games Begin!");
 }, 1000);
 
-var questions = ["The color orange is named after the fruit" , "You can legally drink alcohol while driving in Mississippi" ,"George Washington had wooden teeth","Google was originally called Backrub"]
-for (var i = 0; i < questions.length; i++) {
-  $("#Play").on("click", beginGame);{
+
+var questions = ["The color orange is named after the fruit" , "You can legally drink alcohol while driving in Mississippi" ,"George Washington had wooden teeth","Google was originally called Backrub"]  
+$("#Play").on("click", beginGame);{
     beginGame = setTimeout(function(){
-      $("#questions").append([questions]);
+      var i = 0;
+      $("#questions").empty();
+      $("#questions").append(questions[i]);
+      i++
     }, 2000);
-  }}
+  }
